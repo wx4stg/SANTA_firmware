d2c675f6eb797ee7a66c7349aa006b846c98e3e8
Sam Gardner <samuel.gardner@ttu.edu>
2026-05-25 12:30:33 -0500

    turns out they actually are 0x77

--- temp_collect.py
+++ temp_collect.py
@@ -15,7 +15,7 @@ duration_to_collect = 86400
 
 if __name__ == '__main__':
     i2c = board.I2C()
-    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=118)
+    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=119)
     buffer_len = int(duration_to_collect/sampling_time)
     dt_buf = deque(maxlen=buffer_len)
     temp_buf = deque(maxlen=buffer_len)
