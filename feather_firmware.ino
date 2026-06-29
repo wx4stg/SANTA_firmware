cdbb418e2800ecea9e278d839800410a1ff5ae6d
Sam Gardner <sam@wx4stg.com>
2026-06-29 13:47:37 -0500

    0xE is unreachable, so that was going to 0x0 -> shutdown

--- feather_firmware.ino
+++ feather_firmware.ino
@@ -43,7 +43,6 @@ void setAllRegisters()
   SPI.transfer(0x90); SPI.transfer(0x00); SPI.transfer(0x00); // RESERVED
   SPI.transfer(0x50); // RESERVED
   SPI.transfer(0xA5); // LOCK
-  SPI.transfer(0x00); SPI.transfer(0x0F); // RESERVED
   delay(5);
   digitalWrite(PIN_ADC_CS, HIGH);
 }
