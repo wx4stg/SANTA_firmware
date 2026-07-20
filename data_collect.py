6d2837c4e6eac9a9dedb405fc2124f83b99729cb
Sam Gardner <sam@wx4stg.com>
2026-07-20 12:28:26 -0500

    don't allocate, convert

--- data_collect.py
+++ data_collect.py
@@ -94,12 +94,12 @@ def do_run():
         if 'No such file or directory' in str(e):
             system('sudo shutdown -h now')
     if use_relay == 'a':
-        ser.write(bytes(np.int8(0)))
+        ser.write(np.int8(0).tobytes())
     elif use_relay == 'b':
-        ser.write(bytes(np.int8(1)))
+        ser.write(np.int8(1).tobytes())
     elif use_relay == 'c':
-        ser.write(bytes(np.int8(2)))
-    ser.write(bytes(np.int32(int(cpu_id, 16))))
+        ser.write(np.int8(2).tobytes())
+    ser.write(np.int64(int(cpu_id, 16)).tobytes())
     byte_count_since_last_write = 0
     bytes_data = bytearray()
     ser.flush()
